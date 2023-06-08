bb4aa5394a226138c1701af3da12f53b45ba1340
yms06034 <yms06034@gamil.com>
2023-06-08 10:49:02 +0900

    Doing ch

---
+++ static/js/app.js
@@ -0,0 +1,21 @@
+function checkMobile(){
+	var varUA = navigator.userAgent.toLowerCase();
+	if ( varUA.indexOf('android') > -1) {
+		return "android";
+	} else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+		return "ios";
+	} else {
+		return "other";
+	}
+}
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function() {
+	checkM(checkMobile());
+});
+
+function checkM(m){
+	location.href = 'sms:' +'01046295627'+(m == 'ios' ? '&' : '?') + 'body='+ encodeURIComponent("군포역 트윈비스타 분양 정보 문의합니다.");
+  console.log()
+}
